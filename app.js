a4ba365956e7c290e152ec10782f97af62325d07
T-Thirunilainayagi <thirunilai01@gmail.com>
2021-09-02 20:09:52 +0530

    Data types completed

--- app.js
+++ app.js
@@ -1,12 +1,59 @@
-console.log("Hey this is my first piece of js code!!")
-console.log(299999)
-console.log("thiru")
-console.log(false,333,"thiru",{name:"yadha"})
+// console.log("Hey this is my first piece of js code!!")
+// console.log(299999)
+// console.log("thiru")
+// console.log(false,333,"thiru",{name:"yadha"})
 
-console.log("true")
-console.log("false")
+// console.log("true")
+// console.log("false")
 
 
-console.warn("This is a warning")
+// console.warn("This is a warning")
 
-console.error("This is an error")
+// console.error("This is an error")
+
+// Data types in js
+// Primitive data types
+
+let age=100;
+console.log(age)
+
+// var and let used to declare variables.no need to specify data types
+
+var age1=0999999;
+console.log(typeof age);
+console.log(typeof age1);
+ 
+let Name='ffff4$%';
+console.log(typeof Name);
+
+let brand
+console.log(brand)
+console.log(typeof brand)
+
+// op is undefined bcs its just allocated.bt no value assigned
+
+ brand= null
+console.log(brand)
+console.log(typeof brand)
+
+let istrue=true
+let isfalse=false
+
+
+
+// variables can be overriden. while using second time give only name not var or let keyword.whatever given last taken since js is interpretor language
+
+// Non primitive data types
+
+let arr =[1,2,"44",6]
+arr.push(false)
+arr.push("shiva")
+arr.unshift(01)
+console.log(arr)
+console.log(typeof arr)
+
+
+
+
+
+
