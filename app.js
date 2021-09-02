6ba74b41e1d8b7d65f30197e94468666ebbac44b
T-Thirunilainayagi <thirunilai01@gmail.com>
2021-09-02 19:31:12 +0530

    Console log basics

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("Hey this is my first piece of js code!!")
+console.log(299999)
+console.log("thiru")
+console.log(false,333,"thiru",{name:"yadha"})
+
+console.log("true")
+console.log("false")
+
+
+console.warn("This is a warning")
+
+console.error("This is an error")
